09f23d32afa59f3c79a97e50a061a03385e9a4a9
David Abreu <david.abreu@outlook.com>
2022-01-16 17:50:01 -0300

    analysis complete: top countries which drink alcohol the most!

--- csv_analysis/__main__.py
+++ csv_analysis/__main__.py
@@ -3,8 +3,28 @@ Analysis of alcohol consuming database in CSV by country
 """
 import research
 
+
 def main():
-    research.read_csv()
+    """
+    Starts the application
+    """
+
+    countries_top_beer = research.top_beer(research.read_csv())
+    countries_top_spirit = research.top_spirit(research.read_csv())
+    countries_top_wine = research.top_wine(research.read_csv())
+    countries_top_alcohol = research.top_alcohol(research.read_csv())
+
+    top_countries = {
+        "beer": countries_top_beer,
+        "spirit": countries_top_spirit,
+        "wine": countries_top_wine,
+        "alcohol": countries_top_alcohol,
+    }
+
+    for key, country_list in top_countries.items():
+        print(f"Top 5 Countries which drink {key} the most:\n")
+        for index, row in enumerate(country_list[:10]):
+            print(f"{index+1} | {row.country}")
 
 
 if __name__ == "__main__":
--- csv_analysis/research.py
+++ csv_analysis/research.py
@@ -1,8 +1,15 @@
 """
-Read CSV database, parse and clean the DATA ti be used
+Read CSV database, parse and clean the DATA to be used
 """
 import csv
 import os
+from collections import namedtuple
+
+
+Record = namedtuple(
+    "Record",
+    "country,beer_servings,spirit_servings,wine_servings,total_litres_of_pure_alcohol",
+)
 
 
 def read_csv():
@@ -13,4 +20,36 @@ def read_csv():
     file_name = os.path.join(dir_name, "data", "drinks.csv")
 
     with open(file_name, "r") as f:
-        print(f.read())
+        csv_dict_list = csv.DictReader(f)
+
+        return [parse_csv(row) for row in csv_dict_list]
+
+
+def parse_csv(csv_dict: dict) -> namedtuple:
+    """
+    receives a csv dict, convert the types and return a named tuple
+    """
+
+    for key, cell in csv_dict.items():
+        if "." in cell and cell.replace(".", "", 1).isdigit():
+            csv_dict[key] = float(cell)
+        elif csv_dict[key].isdigit():
+            csv_dict[key] = int(cell)
+
+    return Record(**csv_dict)
+
+
+def top_beer(data):
+    return sorted(data, key=lambda r: r.beer_servings, reverse=True)
+
+
+def top_spirit(data):
+    return sorted(data, key=lambda r: r.spirit_servings, reverse=True)
+
+
+def top_wine(data):
+    return sorted(data, key=lambda r: r.wine_servings, reverse=True)
+
+
+def top_alcohol(data):
+    return sorted(data, key=lambda r: r.total_litres_of_pure_alcohol, reverse=True)
